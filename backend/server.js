9e0f063cd52179b222d203dba11531bdbcca72c7
Thomas Boilesen <thomasboilesen@gmail.com>
2021-05-14 16:23:39 -0300

    mongodb local e site basico funcionando. Proximo passo fazer pagina de checkout.

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+import express from "express";
+import mongoose from "mongoose";
+import userRouter from "./routers/userRouter.js";
+import productRouter from "./routers/productRouter.js";
+
+const app = express();
+mongoose.connect(process.env.MONGODB_URL || "mongodb://localhost/store", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
+
+app.use("/api/users", userRouter);
+app.use("/api/products", productRouter);
+app.get("/", (req, res) => {
+  res.send("Server is ready");
+});
+
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Serve at http://localhost:${port}`);
+});
+
+// mongodb+srv://thomas:<password>@cluster0.bz1fz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
