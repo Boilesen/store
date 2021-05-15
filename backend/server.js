cf70745b561ea16005b1113a7d81781fcf539928
Thomas Boilesen <thomasboilesen@gmail.com>
2021-05-15 15:11:12 -0300

    display orders

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,27 @@
 import express from "express";
 import mongoose from "mongoose";
-import userRouter from "./routers/userRouter.js";
+import dotenv from "dotenv";
 import productRouter from "./routers/productRouter.js";
+import userRouter from "./routers/userRouter.js";
+import orderRouter from "./routers/orderRouter.js";
+
+dotenv.config();
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 mongoose.connect(process.env.MONGODB_URL || "mongodb://localhost/store", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
 });
-
 app.use("/api/users", userRouter);
 app.use("/api/products", productRouter);
+app.use("/api/orders", orderRouter);
+app.get("/api/config/paypal", (req, res) => {
+  res.send(process.env.PAYPAL_CLIENT_ID || "sb");
+});
 app.get("/", (req, res) => {
   res.send("Server is ready");
 });
@@ -24,5 +34,3 @@ const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Serve at http://localhost:${port}`);
 });
-
-// mongodb+srv://thomas:<password>@cluster0.bz1fz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
